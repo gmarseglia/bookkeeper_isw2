546b32009941e98cfcd2cf9a6e4489357c02f3a3
gmarseglia <g.marseglia.it@gmail.com>
2024-12-06 15:20:53 +0100

    Next iteration MyReadCacheTest.java, v2.2.0

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/MyReadCacheTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/MyReadCacheTest.java
@@ -12,6 +12,7 @@ import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
 import java.util.stream.Stream;
 
 @RunWith(Parameterized.class)
@@ -19,6 +20,15 @@ class MyReadCacheTest {
 
     private static final Logger logger = LoggerFactory.getLogger(MyReadCacheTest.class);
     private static final int SEGMENT_SIZE = 64;
+
+    private static void logTest(String testID, KeyStatus keyStatus, String inputString, String expectedString) {
+        logger.info(String.format("#%s: <%s, %s, %s>",
+                testID,
+                keyStatus.toString(),
+                inputString == null ? "null" : String.format("\"%s\"", inputString),
+                expectedString == null ? "null" : String.format("\"%s\"", expectedString)
+        ));
+    }
 
     private static String buildStringOfLength(int n, char c) {
         StringBuilder builder = new StringBuilder();
@@ -30,25 +40,32 @@ class MyReadCacheTest {
 
     private static Stream<Arguments> putAndGetArguments() {
         String empty = "";
-        String smallerThanSS = buildStringOfLength(10, 'a');
+        String smallerThanSS = buildStringOfLength(SEGMENT_SIZE - 1, 'a');
 
         // testID, inputString, keyStatus, expectedString
         return Stream.of(
-                Arguments.of("1", empty, empty),
-                Arguments.of("2", smallerThanSS, smallerThanSS)
+                Arguments.of("1", KeyStatus.WRITE, empty, empty),
+                Arguments.of("2", KeyStatus.WRITE, smallerThanSS, smallerThanSS),
+                Arguments.of("3", KeyStatus.OVERWRITE, empty, empty),
+                Arguments.of("4", KeyStatus.OVERWRITE, "test", "test")
         );
     }
 
     @ParameterizedTest
     @MethodSource("putAndGetArguments")
-    void testPut(String testID, String inputString, String expectedString) {
-        logger.info(String.format("#%s: <%s, %s>",
-                testID,
-                inputString == null ? "null" : String.format("\"%s\"", inputString),
-                expectedString == null ? "null" : String.format("\"%s\"", expectedString)
-        ));
+    void testPut(String testID, KeyStatus keyStatus, String inputString, String expectedString) {
+
+        logTest(testID, keyStatus, inputString, expectedString);
 
         try (ReadCache sut = new ReadCache(UnpooledByteBufAllocator.DEFAULT, 10 * 1024, SEGMENT_SIZE)) {
+
+            if (keyStatus == KeyStatus.OVERWRITE) {
+                String oldString = buildStringOfLength(SEGMENT_SIZE - 1, 'b');
+                ByteBuf old = Unpooled.wrappedBuffer(oldString.getBytes());
+                sut.put(1, 1, old);
+
+                assert Objects.equals(old, sut.get(1, 1));
+            }
 
             if (inputString != null) {
                 ByteBuf input = Unpooled.wrappedBuffer(inputString.getBytes());
@@ -64,5 +81,10 @@ class MyReadCacheTest {
 
             Assertions.assertEquals(expected, actual);
         }
+    }
+
+    public enum KeyStatus {
+        WRITE,
+        OVERWRITE
     }
 }
