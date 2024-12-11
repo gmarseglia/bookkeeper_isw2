1c0ad8081b4fd8de8a2084de609500c0b12384a9
gmarseglia <g.marseglia.it@gmail.com>
2024-12-11 20:05:02 +0100

    Reworked MyReadCacheTest.java, RC_0.1.0

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/MyReadCacheTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/MyReadCacheTest.java
@@ -12,112 +12,47 @@ import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Objects;
 import java.util.stream.Stream;
 
 @RunWith(Parameterized.class)
 class MyReadCacheTest {
 
     private static final Logger logger = LoggerFactory.getLogger(MyReadCacheTest.class);
-    private static final int SEGMENT_SIZE = 256;
-    private static final int SEGMENT_NUMBER = 8;
+    private static final int MAX_CACHE_SIZE = 10 * 1024;
 
-    private static void logTest(String testID, ReadCacheStatus status, String inputString, String expectedString) {
-        logger.info(String.format("#%s: <%s, %s, %s>",
-                testID,
-                status.keyStatus.toString(),
-                inputString == null ? "null" : String.format("\"%s\"", inputString),
-                expectedString == null ? "null" : String.format("\"%s\"", expectedString)
-        ));
-    }
-
-    private static String buildStringOfLength(int n, char c) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < n + 1; i++) {
-            builder.append(c);
-        }
-        return builder.toString();
-    }
 
     private static Stream<Arguments> putAndGetArguments() {
-        String empty = "";
-        String oldString = buildStringOfLength(SEGMENT_SIZE - 2, 'b');
-        String smallerThanSS = buildStringOfLength(SEGMENT_SIZE - 2, 'a');
-        String biggerThanSS = buildStringOfLength(SEGMENT_SIZE + 2, 'a');
-
-        String edgeOldString = buildStringOfLength(4, 'b');
-        String edgeNewString = buildStringOfLength(4, 'a');
-
-        ReadCacheStatus cleanStatus = new ReadCacheStatus(KeyStatus.CLEAN, null);
-        ReadCacheStatus dirtyStatus = new ReadCacheStatus(KeyStatus.DIRTY, oldString);
-        ReadCacheStatus edgeStatus = new ReadCacheStatus(KeyStatus.DIRTY, edgeOldString, (SEGMENT_SIZE * SEGMENT_NUMBER + 1) / 4);
-
-        // String testID, ReadCacheStatus status, String inputString, String expectedString
         return Stream.of(
-                Arguments.of("1", cleanStatus, empty, empty),
-                Arguments.of("2.1", cleanStatus, smallerThanSS, smallerThanSS),
-                Arguments.of("2.2", cleanStatus, biggerThanSS, null),
-                Arguments.of("3", dirtyStatus, empty, empty),
-                Arguments.of("4.1", dirtyStatus, smallerThanSS, smallerThanSS),
-                Arguments.of("4.2", dirtyStatus, biggerThanSS, oldString),
-                Arguments.of("5.1", dirtyStatus, null, oldString),
-                Arguments.of("5.2", edgeStatus, edgeNewString, edgeNewString),
-                Arguments.of("5.3", edgeStatus, null, null)
+                Arguments.of((Object) null, (Object) null),
+                Arguments.of("", ""),
+                Arguments.of("test", "test")
         );
     }
 
     @ParameterizedTest
     @MethodSource("putAndGetArguments")
-    void testPut(String testID, ReadCacheStatus status, String inputString, String expectedString) {
+    void putAndGet(String inputString, String expectedString) {
 
-        logTest(testID, status, inputString, expectedString);
+        try (ReadCache sut = new ReadCache(UnpooledByteBufAllocator.DEFAULT, MAX_CACHE_SIZE)) {
 
-        try (ReadCache sut = new ReadCache(UnpooledByteBufAllocator.DEFAULT, SEGMENT_SIZE * 8, SEGMENT_SIZE)) {
+            ByteBuf inputBuf = null;
+            if (inputString != null)
+                inputBuf = Unpooled.wrappedBuffer(inputString.getBytes());
 
-            if (status.keyStatus == KeyStatus.DIRTY) {
-                ByteBuf old = Unpooled.wrappedBuffer(status.oldString.getBytes());
-                for (int i = 1; i <= status.repetition; i++) {
-                    sut.put(1, i, old);
-                    assert Objects.equals(old, sut.get(1, i));
-                }
+            try {
+                sut.put(1, 1, inputBuf);
+            } catch (NullPointerException e) {
+                Assertions.assertNull(inputString);
+                return;
             }
 
-            if (inputString != null) {
-                ByteBuf input = Unpooled.wrappedBuffer(inputString.getBytes());
-                sut.put(1, 1, input);
-            }
-
-            ByteBuf expected = null;
+            ByteBuf expectedBuf = null;
             if (expectedString != null) {
-                expected = Unpooled.wrappedBuffer(expectedString.getBytes());
+                expectedBuf = Unpooled.wrappedBuffer(expectedString.getBytes());
             }
 
-            ByteBuf actual = sut.get(1, 1);
-
-            Assertions.assertEquals(expected, actual);
+            Assertions.assertEquals(expectedBuf, sut.get(1, 1));
         }
     }
 
-    public enum KeyStatus {
-        CLEAN,
-        DIRTY
-    }
-
-    public static class ReadCacheStatus {
-        public KeyStatus keyStatus;
-        public String oldString;
-        public int repetition;
-
-        public ReadCacheStatus(KeyStatus keyStatus, String oldString) {
-            this.keyStatus = keyStatus;
-            this.oldString = oldString;
-            this.repetition = 1;
-        }
-
-        public ReadCacheStatus(KeyStatus keyStatus, String oldString, int repetition) {
-            this.keyStatus = keyStatus;
-            this.oldString = oldString;
-            this.repetition = repetition;
-        }
-    }
 }
