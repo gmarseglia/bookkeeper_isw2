e99957d6b468a2782309ac92fe7a6efd67abf5a1
gmarseglia <g.marseglia.it@gmail.com>
2025-01-21 18:01:38 +0100

    GitHub workflows: updated results_workflow.yml

--- bookkeeper-server/results/runAndSave.sh
+++ bookkeeper-server/results/runAndSave.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
-if [[ $# -lt 1 ]]
+if [[ $# -lt 2 ]]
 then
-	echo "Incorrect usage."
+	echo "Incorrect usage. Correct usage: runAndSave.sh <version_name> <--rc or --bf or --all> [-p]"
 	exit 1
 fi
 
 PITEST=false
+READ_CACHE=false
+BUFFERED_CHANNEL=false
+ALL=false
+ACTIVE_PROFILE=""
 
 for arg in "$@"
 do
-	if [ "$arg" == "-p" ]
-	then
+	if [ "$arg" == "-p" ] ; then
 		PITEST=true
 	fi
+	if [ "$arg" == "--rc" ] ; then
+    READ_CACHE=true
+    ACTIVE_PROFILE="MyReadCacheSuite"
+  fi
+  if [ "$arg" == "--bf" ] ; then
+    BUFFERED_CHANNEL=true
+    ACTIVE_PROFILE="MyBufferedChannelSuite"
+  fi
+  if [ "$arg" == "--all" ] ; then
+    ALL=true
+    ACTIVE_PROFILE="MyAllSuite"
+  fi
 done
+
+if [ $READ_CACHE == false ] && [ $BUFFERED_CHANNEL == false ] && [ $ALL == false ] ; then
+  echo "Incorrect usage. Correct usage: runAndSave.sh <version_name> <--rc or --bf or --all> [-p]"
+  exit 1
+fi
 
 cd ..
 mvn clean
-mvn test -Pjacoco,ignoreTestFailure,customTest
-mvn test -Pbadua,ignoreTestFailure,customTest
+mvn test -Pjacoco,ignoreTestFailure,customTest,$ACTIVE_PROFILE
+mvn test -Pbadua,ignoreTestFailure,customTest,$ACTIVE_PROFILE
 if $PITEST
 then
-	mvn test -Ppitest,ignoreTestFailure,customTest
+	mvn test -Ppitest,ignoreTestFailure,customTest,$ACTIVE_PROFILE
 fi
 
 cd results/ || exit
