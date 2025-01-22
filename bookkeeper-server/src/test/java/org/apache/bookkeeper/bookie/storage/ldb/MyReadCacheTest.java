906e4a2ebf1265857c06a53fa1532ba9c151ccfb
gmarseglia <g.marseglia.it@gmail.com>
2025-01-22 18:02:09 +0100

    RC_1.2.1: first version of concurrentTest working

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/MyReadCacheTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/MyReadCacheTest.java
@@ -1,7 +1,9 @@
 package org.apache.bookkeeper.bookie.storage.ldb;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -9,10 +11,15 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Stream;
+
+import static org.mockito.Mockito.*;
 
 class MyReadCacheTest {
 
@@ -153,6 +160,127 @@ class MyReadCacheTest {
                     expectedEntry.content.internalNioBuffer(0, size),
                     actual.internalNioBuffer(0, size));
 
+            actual.release();
+        }
+    }
+
+    @Test
+    void putConcurrentTest() throws NoSuchFieldException, IllegalAccessException, InterruptedException {
+        Configuration firstFull = new Configuration(SegmentState.FULL, SegmentState.EMPTY);
+        TestState testState = new TestState(
+                "#09: concurrent test",
+                firstFull,
+                CompositeIdState.NON_PRESENT, EntryState.LESS_EQUAL_THAN_ACTUAL_SEGMENT_CAPACITY_LOW,
+                EnumSet.of(
+                        ExpectedFlag.PRIOR_ENTRIES_READABLE),
+                false
+        );
+
+        logger.info(testState.description);
+
+        MyReadCacheTestConfigurer configurer = new MyReadCacheTestConfigurer();
+        configurer.setup(testState);
+
+        String debugMsg = String.format(
+                "ledgerId: %d, entryId: %d, entry.writerIndex(): %s",
+                testState.ledgerId,
+                testState.entryId,
+                testState.entry == null ? "null" : testState.entry.writerIndex());
+        logger.info(debugMsg);
+
+        /* Set up the mocked lock */
+        ReentrantReadWriteLock spyReentrantReadWriteLock = spy(ReentrantReadWriteLock.class);
+        ReentrantReadWriteLock.WriteLock mockWriteLock = mock(ReentrantReadWriteLock.WriteLock.class);
+
+        // semaphore really implements the synchronization between the threads
+        Semaphore semaphore = new Semaphore(1);
+        semaphore.drainPermits();
+
+        // The first thread to ask for the lock, will be blocked until the second has released it
+        doAnswer(invocationOnMock -> {
+            logger.info("1st wants to lock");
+            semaphore.acquire();
+            logger.info("1st has received");
+            return null;
+        }).doAnswer(invocationOnMock -> {
+            logger.info("2nd wants to lock");
+            logger.info("2nd has received");
+            return null;
+        }).when(mockWriteLock).lock();
+
+        // first thread to release, will release it
+        doAnswer(invocationOnMock -> {
+            logger.info("2nd wants to unlock");
+            semaphore.release();
+            logger.info("2nd has released");
+            return null;
+        }).doAnswer(invocationOnMock -> {
+            logger.info("1st wants to unlock");
+            logger.info("1st has released");
+            return null;
+        }).when(mockWriteLock).unlock();
+
+        // Mock the writeLock() method to return the mocker writeLock
+        doReturn(mockWriteLock).when(spyReentrantReadWriteLock).writeLock();
+
+        /* Mock the internal lock via reflection */
+        ReadCache sut = testState.sut;
+        Field lockField = ReadCache.class.getDeclaredField("lock");
+        lockField.setAccessible(true);
+        lockField.set(sut, spyReentrantReadWriteLock);
+
+        /* Create the entries */
+        long aEntryId = testState.entryId;
+        long bEntryId = aEntryId + 1;
+        int expectedSize = testState.entry.writerIndex();
+        ByteBuf aEntry = Unpooled.buffer(expectedSize);
+        ByteBuf bEntry = Unpooled.buffer(expectedSize);
+        for (int i = 0; i < expectedSize; i++) {
+            aEntry.writeByte((byte) (testState.ledgerId * 10 + aEntryId));
+            bEntry.writeByte((byte) (testState.ledgerId * 10 + bEntryId));
+        }
+        testState.entry.release();
+
+        /* Create and launch the threads */
+        Thread threadA = new Thread(() -> sut.put(testState.ledgerId, aEntryId, aEntry));
+        Thread threadB = new Thread(() -> sut.put(testState.ledgerId, bEntryId, bEntry));
+        threadA.start();
+        threadB.start();
+
+        /* Wait for the thread to complete */
+        threadB.join();
+        threadA.join();
+
+        /* Assert first */
+        ByteBuf aEntryActual = sut.get(testState.ledgerId, aEntryId);
+        Assertions.assertEquals(
+                aEntry.internalNioBuffer(0, expectedSize),
+                aEntryActual.internalNioBuffer(0, expectedSize)
+        );
+        aEntryActual.release();
+        aEntry.release();
+
+        /* Assert second */
+        ByteBuf bEntryActual = sut.get(testState.ledgerId, bEntryId);
+        Assertions.assertEquals(
+                bEntry.internalNioBuffer(0, expectedSize),
+                bEntryActual.internalNioBuffer(0, expectedSize)
+        );
+        bEntryActual.release();
+        bEntry.release();
+
+        /* Assert on the expectedEntries */
+        logger.info(String.format("# of assertEquals expected: %d", testState.expectedEntries.size()));
+        for (MyReadCacheTestEntry expectedEntry : testState.expectedEntries) {
+            int size = expectedEntry.content.writerIndex();
+            ByteBuf actual = testState.sut.get(expectedEntry.ledgerId, expectedEntry.entryId);
+            logger.info(String.format("actual: %s", actual.toString()));
+
+            Assertions.assertEquals(
+                    expectedEntry.content.internalNioBuffer(0, size),
+                    actual.internalNioBuffer(0, size));
+
+            expectedEntry.content.release();
             actual.release();
         }
     }
